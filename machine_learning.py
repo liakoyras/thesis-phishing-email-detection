97141b728afee3dcd7751a0553d0280ef48c3e9e
Ilias Chanis <iliashanis@gmail.com>
2022-08-21 18:40:04 +0300

    Add wrappers for more classifiers
    Added functions to train Random Forest, Decision Tree, Gradient
    Boosting and Naive Bayes classifiers.
    
    Also, created a new global variable for random_state that will be
    used by all algorithms, instead of specifying it every time.

--- machine_learning.py
+++ machine_learning.py
@@ -8,6 +8,10 @@ Based on scikit-learn.
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.naive_bayes import MultinomialNB
 
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import precision_score
@@ -15,6 +19,8 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import roc_auc_score
+
+alg_random_state = 1746
 
 """
 Preprocessing
@@ -119,10 +125,163 @@ def train_logistic_regression(features, target, max_iter=500, penalty='l2', C=1e
     --------
     fit_model : Fit a classifier.
     """
-    lr = LogisticRegression(max_iter=max_iter, penalty=penalty, C=C, random_state=1746)
+    lr = LogisticRegression(max_iter=max_iter, penalty=penalty, C=C, random_state=alg_random_state)
     fitted_lr = fit_model(lr, features, target, show_train_accuracy)
     
     return fitted_lr
+
+def train_decision_tree(features, target, max_depth=5, show_train_accuracy=False):
+    """
+    Train a Decision Tree classifier.
+    
+    It is a simple wrapper that creates an
+    sklearn.DecisionTreeClassifier model using the input
+    parameters and then uses fit_model() to train it.
+    
+    Parameters
+    ----------
+    features : pandas.DataFrame
+        The DataFrame containing the features that the
+        classifier will be fitted on.
+    target : pandas.Series
+        The Series with the target class variable.
+    max_depth : int, default 5
+        Maximum depth of the tree.
+        To be used by DecisionTreeClassifier.
+    show_train_accuracy : bool, default False
+        If True, it prints the accuracy of the model
+        on the training data. To be used by fit_model().
+        
+    Returns
+    -------
+    sklearn.tree._classes.DecisionTreeClassifier
+        The fitted DecisionTreeClassifier classifier.
+        
+    See Also
+    --------
+    fit_model : Fit a classifier.
+    """
+    dt = DecisionTreeClassifier(max_depth=max_depth, random_state=alg_random_state)
+    fitted_dt = fit_model(dt, features, target, show_train_accuracy)
+    
+    return fitted_dt
+
+def train_random_forest(features, target, max_depth=5, n_estimators=20, show_train_accuracy=False):
+    """
+    Train a Random Forest classifier.
+    
+    It is a simple wrapper that creates an
+    sklearn.RandomForestClassifier model using the input
+    parameters and then uses fit_model() to train it.
+    
+    Parameters
+    ----------
+    features : pandas.DataFrame
+        The DataFrame containing the features that the
+        classifier will be fitted on.
+    target : pandas.Series
+        The Series with the target class variable.
+    max_depth : int, default 5
+        Maximum depth of the tree.
+        To be used by RandomForestClassifier.
+    n_estimators : int, default 20
+        The number of trees that will be used.
+        To be used by RandomForestClassifier.
+    show_train_accuracy : bool, default False
+        If True, it prints the accuracy of the model
+        on the training data. To be used by fit_model().
+        
+    Returns
+    -------
+    sklearn.ensemble._forest.RandomForestClassifier
+        The fitted RandomForestClassifier classifier.
+        
+    See Also
+    --------
+    fit_model : Fit a classifier.
+    """
+    rf = RandomForestClassifier(max_depth=max_depth, n_estimators=n_estimators, random_state=alg_random_state)
+    fitted_rf = fit_model(rf, features, target, show_train_accuracy)
+    
+    return fitted_rf
+
+def train_gradient_boost(features, target, loss='log_loss', max_depth=3, learning_rate=0.1, show_train_accuracy=False):
+    """
+    Train a Gradient Boosting classifier.
+    
+    It is a simple wrapper that creates an
+    sklearn.GradientBoostingClassifier model using the input
+    parameters and then uses fit_model() to train it.
+    
+    Parameters
+    ----------
+    features : pandas.DataFrame
+        The DataFrame containing the features that the
+        classifier will be fitted on.
+    target : pandas.Series
+        The Series with the target class variable.
+    loss : str, default 'log_loss'
+        The loss function to be optimized. To be used
+        by GradientBoostingClassifier.
+    max_depth : int, default 3
+        Maximum depth of the individual estimators. To be
+        used by GradientBoostingClassifier.
+    learning_rate : float, default 0.1
+        Reduces the contribution of each tree by this amount.
+        To be used by GradientBoostingClassifier.
+    show_train_accuracy : bool, default False
+        If True, it prints the accuracy of the model
+        on the training data. To be used by fit_model().
+        
+    Returns
+    -------
+    sklearn.ensemble._gb.GradientBoostingClassifier
+        The fitted GradientBoostingClassifier classifier.
+        
+    See Also
+    --------
+    fit_model : Fit a classifier.
+    """
+    gb = GradientBoostingClassifier(loss=loss, max_depth=max_depth, learning_rate=learning_rate, random_state=alg_random_state)
+    fitted_gb = fit_model(gb, features, target, show_train_accuracy)
+    
+    return fitted_gb
+
+def train_naive_bayes(features, target, alpha=1.0, show_train_accuracy=False):
+    """
+    Train a Multinomial Naive Bayes classifier.
+    
+    It is a simple wrapper that creates an
+    sklearn.MultinomialNB model using the input
+    parameters and then uses fit_model() to train it.
+    
+    Parameters
+    ----------
+    features : pandas.DataFrame
+        The DataFrame containing the features that the
+        classifier will be fitted on.
+    target : pandas.Series
+        The Series with the target class variable.
+    alpha : float, default 1.0
+        The parameter for additive smoothing. To be used
+        by MultinomialNB.
+    show_train_accuracy : bool, default False
+        If True, it prints the accuracy of the model
+        on the training data. To be used by fit_model().
+        
+    Returns
+    -------
+    sklearn.naive_bayes.MultinomialNB
+        The fitted MultinomialNB classifier.
+        
+    See Also
+    --------
+    fit_model : Fit a classifier.
+    """
+    nb = MultinomialNB(alpha=alpha)
+    fitted_nb = fit_model(nb, features, target, show_train_accuracy)
+    
+    return fitted_nb
 
 
 """
